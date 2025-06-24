fc06050725e25dff9d4e294038bd756a362af39c
SongMinQQ <107177636+SongMinQQ@users.noreply.github.com>
2025-06-24 14:47:37 +0900

    CHAM-100 DESIGN: 다크 모드에서의 가독성 향상

--- components/chat/TypingIndicator.tsx
+++ components/chat/TypingIndicator.tsx
@@ -1,9 +1,9 @@
 export default function TypingIndicator() {
   return (
     <div className="flex items-center space-x-1 px-2">
-      <span className="w-2 h-2 bg-gray-400 dark:bg-gray-600 rounded-full animate-blink"></span>
-      <span className="w-2 h-2 bg-gray-400 dark:bg-gray-600 rounded-full animate-blink delay-200"></span>
-      <span className="w-2 h-2 bg-gray-400 dark:bg-gray-600 rounded-full animate-blink delay-400"></span>
+      <span className="w-2 h-2 bg-gray-400 dark:bg-gray-200 rounded-full animate-blink"></span>
+      <span className="w-2 h-2 bg-gray-400 dark:bg-gray-200 rounded-full animate-blink delay-200"></span>
+      <span className="w-2 h-2 bg-gray-400 dark:bg-gray-200 rounded-full animate-blink delay-400"></span>
     </div>
   );
 }
